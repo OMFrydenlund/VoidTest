939bdc9af28b3d17c71caf2149a5b6c4506fb4db
OMFrydenlund <migfrydenlund@gmail.com>
2023-11-15 14:34:15 +0100

    Unsure, get back to it. Added comments

--- Program.cs
+++ Program.cs
@@ -2,13 +2,14 @@
 
 class Program
 {
-    static void Main()
+    static void Main() //"void" used to associate with the class and not a specific instance (object) of said class
     {
         WriteMessage();
         Console.ReadLine();
     }
-    static void WriteMessage()
+    static void WriteMessage() //see void comment above
     {
-        Console.WriteLine("Denne metoden returnerer ingenting");
+        Console.WriteLine("Denne metoden returnerer ingenting"); //program window displays and behaves differently depending on
+        // whether you called WriteLine directly in Main or through adding and calling another method (WriteMessage)
     }
 }
