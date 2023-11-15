c82981fa01f08166c9a7aec03dc16d83a7324640
OMFrydenlund <migfrydenlund@gmail.com>
2023-11-15 14:18:57 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        WriteMessage();
+        Console.ReadLine();
+    }
+    static void WriteMessage()
+    {
+        Console.WriteLine("Denne metoden returnerer ingenting");
+    }
+}
